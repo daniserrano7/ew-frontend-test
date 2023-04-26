90708253f5569d346a932043e681a63e35dd0ff6
Daniel Serrano <danielserrano.contacto@gmail.com>
2023-04-26 17:14:14 +0200

    feat: return movies ranked by genres matches

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,29 @@ export const getFilteredMovies = ({ genres }: { genres: Genre[] }): Movie[] => {
     return [randomMovie];
   }
 
-  return [];
+  // Array of arrays of movies, where each array is a level of genre matches
+  // e.g. filtering with 3 genres: [ [movies with 3 matches], [movies with 2 matches], [movies with 1 match] ]
+  const moviesRankedByMatches = Array.from({ length: genres.length }).map(
+    () => []
+  ) as Movie[][];
+
+  movies.forEach((movie: Movie) => {
+    if (movie.genres.length === 0) return;
+
+    // Check if all movie genres are included in the genres filter
+    const allMovieGenresMatches = movie.genres.every((genre: string) =>
+      genres.includes(genre as Genre)
+    );
+
+    // If all genres matches, we include the movie in the array of movies with the same number of matches
+    if (allMovieGenresMatches) {
+      const level = movie.genres.length - 1;
+      moviesRankedByMatches[level].push(movie);
+    }
+  });
+
+  // Given that we want to prioritize movies with more matches, we reverse the array and flatten it
+  // to get a single array of movies from the most matched to the least matched
+  const rankedMovies = moviesRankedByMatches.slice().reverse().flat();
+  return rankedMovies;
 };
