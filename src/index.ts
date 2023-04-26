264d369850f9ab8ae08ca22a836aa73d8e8bf327
Daniel Serrano <danielserrano.contacto@gmail.com>
2023-04-26 15:58:10 +0200

    feat: return random movie when empty genres

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { Genre, Movie } from "./index.types";
+const moviesDb = require("./db.json").movies as Movie[];
+
+// Simulates a database call
+const getMovies = (): Movie[] => moviesDb;
 
 export const getFilteredMovies = ({ genres }: { genres: Genre[] }): Movie[] => {
+  const movies = getMovies();
+  const genresIsEmpety = genres.length === 0;
+
+  if (genresIsEmpety) {
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    const randomMovie = movies[randomIndex];
+    return [randomMovie];
+  }
+
   return [];
 };
