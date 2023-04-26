d024b2ef85b8bb087e5c0f4543fcdc9fbe2321ca
Daniel Serrano <danielserrano.contacto@gmail.com>
2023-04-26 22:16:03 +0200

    perf: replace array iterations with for loop

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,7 +1,17 @@
 import { getFilteredMovies } from "./index";
+import { Movie } from "./index.types";
+const moviesDb = require("./db.json").movies as Movie[];
 
+// This is the function used to generate the mock data for the benchmark
+export const mockGetMovies = (): Movie[] =>
+  Array.from({ length: 1000 })
+    .map(() => moviesDb)
+    .flat();
+
+// This is the code used to benchmark the getFilteredMovies function
+const n = 10;
 let mean = 0;
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < n; i++) {
   const start = performance.now();
   getFilteredMovies({ genres: ["Crime", "Drama", "Music"] });
   const end = performance.now();
@@ -10,4 +20,4 @@ for (let i = 0; i < 10; i++) {
   console.log(`Execution time: ${time} ms`);
 }
 
-console.log(`Mean execution time: ${mean / 10} ms`);
+console.log(`Mean execution time: ${mean / n} ms`);
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,8 @@ const moviesDb = require("./db.json").movies as Movie[];
 // Simulates a database call
 const getMovies = (): Movie[] => moviesDb;
 
-// Larget dataset to test performance
-export const mockGetMovies = (): Movie[] =>
-  Array.from({ length: 1000 })
-    .map(() => moviesDb)
-    .flat();
-
 export const getFilteredMovies = ({ genres }: { genres: Genre[] }): Movie[] => {
   const movies = getMovies();
-  // const movies = mockGetMovies();
   const genresSet = new Set(genres);
   const genresIsEmpety = genresSet.size === 0;
 
@@ -24,27 +17,31 @@ export const getFilteredMovies = ({ genres }: { genres: Genre[] }): Movie[] => {
 
   // Array of arrays of movies, where each array is a level of genre matches
   // e.g. filtering with 3 genres: [ [movies with 3 matches], [movies with 2 matches], [movies with 1 match] ]
-  const moviesRankedByMatches = Array.from({ length: genres.length }).map(
-    () => []
-  ) as Movie[][];
+  const moviesRankedByMatches = new Array(genres.length);
+  for (let i = 0; i < genres.length; i++) {
+    moviesRankedByMatches[i] = [];
+  }
 
-  movies.forEach((movie: Movie) => {
-    if (movie.genres.length === 0) return;
+  for (const movie of movies) {
+    if (movie.genres.length === 0) continue;
 
     // Check if all movie genres are included in the genres filter
     const allMovieGenresMatches = movie.genres.every((genre: string) =>
-      genres.includes(genre as Genre)
+      genresSet.has(genre as Genre)
     );
 
     // If all genres matches, we include the movie in the array of movies with the same number of matches
     if (allMovieGenresMatches) {
-      const level = movie.genres.length - 1;
-      moviesRankedByMatches[level].push(movie);
+      const matches = movie.genres.length - 1;
+      moviesRankedByMatches[matches].push(movie);
     }
-  });
+  }
 
   // Given that we want to prioritize movies with more matches, we reverse the array and flatten it
   // to get a single array of movies from the most matched to the least matched
-  const rankedMovies = moviesRankedByMatches.reverse().flat();
+  const rankedMovies = [];
+  for (let i = moviesRankedByMatches.length - 1; i >= 0; i--) {
+    rankedMovies.push(...moviesRankedByMatches[i]);
+  }
   return rankedMovies;
 };
